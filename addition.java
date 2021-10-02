b20c385fdc57d7a2b4f8580d39c35a227e17ab7b
Yash Shendge <75063323+Yash-Hax@users.noreply.github.com>
2021-10-02 14:49:34 +0530

    Update addition.java

--- addition.java
+++ addition.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+```import java.util.Scanner;
 
 public class addition {
     public static void main(String[] args) {
@@ -8,4 +8,4 @@ public class addition {
         int b=inp.nextInt();
         System.out.println("Addition of integers is: "+(a+b));
     }
-}
+}```
